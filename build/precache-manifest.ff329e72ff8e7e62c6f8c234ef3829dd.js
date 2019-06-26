0a3453c35ecc6e848dadda5cfce9ec6dd0878f8f
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-06-26 00:00:19 -0400

    fix

---
+++ build/precache-manifest.ff329e72ff8e7e62c6f8c234ef3829dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623b2fc3ab0d2aa34013b437383c6243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43efe2e32fb3698f71a2",
+    "url": "/static/css/main.b4790c57.chunk.css"
+  },
+  {
+    "revision": "2f0f58acf8a9b5f225b2",
+    "url": "/static/js/2.caa000a1.chunk.js"
+  },
+  {
+    "revision": "43efe2e32fb3698f71a2",
+    "url": "/static/js/main.a9a52e3e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
